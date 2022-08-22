2144a3b2140735631703a393c5747be370ad9e47
Ari-chan <arielle.elak@gmail.com>
2022-08-21 21:24:45 -0600

    Testing functionality of ids on HTML page and appendChild to generate the options

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,19 @@
 // Last Updated by Arielle Schlickman-Elak Aug 21 2022
 
 // Variables for all components of HTML page
-var viewHighscores = document.querySelector("#viewHighscores");
+var viewHighscores = document.querySelector("#view-highscores");
 var timer = document.querySelector("#timer");
-var questionTitle = document.querySelector("#questionTitle");
-var optionsList = document.querySelector("#optionsList");
-var validation = document.querySelector("#validation");
-var endGame = document.querySelector("#endGame");
-var initialsArea = document.querySelector("#initialsArea");
 
+var validationSection = document.querySelector("#validation");
+var endGameSection = document.querySelector("#end-game");
+var initialsArea = document.querySelector("#initials-area");
+var initials = document.querySelector("initials");
+var submitButton = document.querySelector("#submit");
+var goBackButton = document.querySelector("#go-back");
+var clearScoreButton = document.querySelector("#clear-highscores");
 
+// Variable for where question will appear on page
+var questionTitle = document.querySelector("#question-title");
 // Set up variables with associations to the four different option spaces on the page
 var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
@@ -18,8 +22,39 @@ var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
 
 
-// Set up quiz questions with an equivalent index that can be shuffled (so that no two tests will be the same)
-// With each question and answer set up as a JSON object with an attribute boolean of true/false
-const quizQuestions = {
+var questionInput = "Test question.";
+var option1Input = "Test option1";
+var option2Input = "Test option2";
+var option3Input = "Test option3";
+var option4Input = "Test option4";
 
+var answerInput = "Test answer";
+
+// Create a variable to house the randomly selected question
+var quizQuestion = {
+    question: questionInput.value,
+    options: [option1Input, option2Input, option3Input, option4Input],
+    answer: answerInput
+};
+
+// Select the options-list id in HTML for where the options will populate
+
+var test = document.querySelector("#test");
+
+function createOption(option) {
+    let li = document.createElement('li');
+    li.textContent = option;
+    return li;
 }
+// Get the options-list UL on the page
+const optionsList = document.querySelector('#options-list');
+
+// Add a new option
+optionsList.appendChild(createOption('Option 1'));
+optionsList.appendChild(createOption('Option 2'));
+optionsList.appendChild(createOption('Option 3'));
+optionsList.appendChild(createOption('Option 4'));
+
+
+// Add listener to check function
+test.addEventListener("click", createOption);
