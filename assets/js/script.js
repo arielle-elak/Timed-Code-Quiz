9584dbcb5535d29b6f7d22e09ab331fc2b4fb5b1
Ari-chan <arielle.elak@gmail.com>
2022-08-21 20:19:22 -0600

    Initial structure of HTML page and variables

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Timed Coding Quiz
+// Last Updated by Arielle Schlickman-Elak Aug 21 2022
+
+// Set up variables with associations to the four different option spaces on the page
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
+
+
+// Set up quiz questions with an equivalent index that can be shuffled (so that no two tests will be the same)
+// With each question and answer set up as a JSON object with an attribute boolean of true/false
+const quizQuestions = {
+
+}
