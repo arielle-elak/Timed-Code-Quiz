872f4e92dfb0ab3198e2f97e1d6777f7db8b10cd
Ari-chan <arielle.elak@gmail.com>
2022-08-21 20:36:42 -0600

    Mapping out HTML sections to show and hide for different conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 // Timed Coding Quiz
 // Last Updated by Arielle Schlickman-Elak Aug 21 2022
 
+// Variables for all components of HTML page
+var viewHighscores = document.querySelector("#viewHighscores");
+var timer = document.querySelector("#timer");
+var questionTitle = document.querySelector("#questionTitle");
+var optionsList = document.querySelector("#optionsList");
+var validation = document.querySelector("#validation");
+var endGame = document.querySelector("#endGame");
+var initialsArea = document.querySelector("#initialsArea");
+
+
 // Set up variables with associations to the four different option spaces on the page
 var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
